9761e738eda41ddd99b504c8c1fee1a862629d98
Mert Ünal <mertunal8877@gmail.com>
2024-11-07 14:26:56 +0300

    Java9
    0 ile 100 arasındaki tüm çift sayıları gösteren uygulama

---
+++ Sayılar.java
@@ -0,0 +1,14 @@
+
+public class Sayılar {
+
+	public static void main(String[] args) {
+		int sayı =0;
+		while(sayı <=100) {
+		if(sayı %2 !=1)
+			System.out.println(sayı);
+		sayı++;
+		} 
+
+	} 
+
+} 
